be172629adfab5a914b98630d9f5c42110b554dd
Kum <azharisaputrasikumbang@gmail.com>
2025-05-12 17:41:14 +0700

    add frontend component

---
+++ resources/js/components/ui/button/ButtonBack.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Button from './Button.vue';
+
+const backButton = () => window.history.back();
+</script>
+
+<template>
+  <Button variant="destructive" as="a" @click="backButton">
+    <slot />
+  </Button>
+</template>
